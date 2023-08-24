79caf672017924a163799d33b2dfaab056fd2e30
DaviLimS <davilimasoares1@outlook.com>
2023-08-24 11:20:15 -0300

    test

---
+++ control.js
@@ -0,0 +1,21 @@
+function music() {
+    alert("Eu só quero que saiba o quanto eu gosto de compartilhar e conhecer sobre seus gostos musicais :)");
+}
+function ship() {
+    alert("Como você já sabe, uma coisa que me marcou, por mais boba que possa parecer, foi aquele barquinho que você fez pra mim!");
+}
+function kidness() {
+    alert("E eu também AMO o quão você é carinhosa e atenciosa mesmo que do seu jeito kkkkk");
+}
+function cat() {
+    alert("Eu ainda desenharei vários gatinhos no seu caderno e nas suas coisas, já estou praticando bb, me aguarde kkkkkk");
+}
+function paint() {
+    alert("Eu já te falei o quão eu acho seu traço lindo?");
+}
+function drawn() {
+    alert("Ele é simplismente incrível, nem preciso falar que não era pra menos né? kkkkkk");
+}
+function friend() {
+    alert("Porfim eu quero que saiba que cada palavra dita aqui é a verdade pelo menos para mim, e que eu queria converça mais com você, pode pelo menos mandar umas mensagens de vez em quando e ir converçar na comigo, porfavor? kkkkkk tmj");
+}
